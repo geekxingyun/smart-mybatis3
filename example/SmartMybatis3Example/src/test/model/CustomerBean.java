f974df503a81fa4274554dd6ce8832e5b9435781
fairy_xingyun <fairy_xingyun@hotmail.com>
2017-04-09 11:14:17 +0800

    modify model class

--- example/SmartMybatis3Example/src/test/model/CustomerBean.java
+++ example/SmartMybatis3Example/src/test/model/CustomerBean.java
@@ -8,94 +8,17 @@ public class CustomerBean implements Serializable{
 	 *   Model
 	 */
 	private static final long serialVersionUID = -6894246401229699827L;
-	private Long customerId;// 用户Id required auto Create
-	private String customerAccount;// 用户名 用户帐号 手机号 required
-	private String customerPassword;// 用户密码 required
-	private String customerNikeName;// 用户昵称
-	private String customerToken;// 验证身份Token 由账号和随机数MD5加密生成
-	private String customerPhoto;// 用户头像
-	private Integer customerSex;// 用户性别 0 保密 1 男 2女
-	private Long customerScore;// 用户积分
-	private Long customerLevelId;// 用户级别Id
-	private String customerRealName;// 用户真实姓名
-	private Integer customerAge;// 用户年龄
-	private Long customerRegisterTime;// 用户注册时间 auto create
-	public Long getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
-	public String getCustomerAccount() {
-		return customerAccount;
-	}
-	public void setCustomerAccount(String customerAccount) {
-		this.customerAccount = customerAccount;
-	}
-	public String getCustomerPassword() {
-		return customerPassword;
-	}
-	public void setCustomerPassword(String customerPassword) {
-		this.customerPassword = customerPassword;
-	}
-	public String getCustomerNikeName() {
-		return customerNikeName;
-	}
-	public void setCustomerNikeName(String customerNikeName) {
-		this.customerNikeName = customerNikeName;
-	}
-	public String getCustomerToken() {
-		return customerToken;
-	}
-	public void setCustomerToken(String customerToken) {
-		this.customerToken = customerToken;
-	}
-	public String getCustomerPhoto() {
-		return customerPhoto;
-	}
-	public void setCustomerPhoto(String customerPhoto) {
-		this.customerPhoto = customerPhoto;
-	}
-	public Integer getCustomerSex() {
-		return customerSex;
-	}
-	public void setCustomerSex(Integer customerSex) {
-		this.customerSex = customerSex;
-	}
-	public Long getCustomerScore() {
-		return customerScore;
-	}
-	public void setCustomerScore(Long customerScore) {
-		this.customerScore = customerScore;
-	}
-	public Long getCustomerLevelId() {
-		return customerLevelId;
-	}
-	public void setCustomerLevelId(Long customerLevelId) {
-		this.customerLevelId = customerLevelId;
-	}
-	public String getCustomerRealName() {
-		return customerRealName;
-	}
-	public void setCustomerRealName(String customerRealName) {
-		this.customerRealName = customerRealName;
-	}
-	public Integer getCustomerAge() {
-		return customerAge;
-	}
-	public void setCustomerAge(Integer customerAge) {
-		this.customerAge = customerAge;
-	}
-	public Long getCustomerRegisterTime() {
-		return customerRegisterTime;
-	}
-	public void setCustomerRegisterTime(Long customerRegisterTime) {
-		this.customerRegisterTime = customerRegisterTime;
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
+	public Long customerId;// 用户Id required auto Create
+	public String customerAccount;// 用户名 用户帐号 手机号 required
+	public String customerPassword;// 用户密码 required
+	public String customerNikeName;// 用户昵称
+	public String customerToken;// 验证身份Token 由账号和随机数MD5加密生成
+	public String customerPhoto;// 用户头像
+	public Integer customerSex;// 用户性别 0 保密 1 男 2女
+	public Long customerScore;// 用户积分
+	public Long customerLevelId;// 用户级别Id
+	public String customerRealName;// 用户真实姓名
+	public Integer customerAge;// 用户年龄
+	public Long customerRegisterTime;// 用户注册时间 auto create
+
 }
